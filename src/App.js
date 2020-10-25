9f543b5307e103733019002c9b160ef014510270
zidcoder <ozidhalawa97@gmail.com>
2020-10-25 18:12:54 +0700

    add all files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "assets/scss/style.scss";
+import Button from "./index.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Button></Button>
+    </div>
+  );
+}
+
+export default App;
